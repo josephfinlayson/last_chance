e3b77a20e7e30b2d957d6a2eb7d09add9dd241c7
Joseph Finlaysonlinux <joseph.finlayson@gmail.com>
2012-05-17 22:36:07 +0100

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastChance::Application.config.secret_token = '58562ff32c1a4b8a3d8f58f3932e0d27628272437ceb95d73fbb569affb7f1dae82653642b470212ac7d0fd67714bb1f5a41bb7ae47da248f3503fe257cc4248'
